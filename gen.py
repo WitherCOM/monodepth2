5675dbb47df5d138a5d2d2eac06618b875418b4f
Németh Csaba Tibor <nemeth.csaba.tibor@gmail.com>
2023-06-03 13:41:21 +0200

    Fix loader

--- gen.py
+++ gen.py
@@ -3,16 +3,16 @@ import sys
 import cv2
 from datetime import datetime
 
-if not os.path.isfile(f'./videos/{sys.argv[1]}'):
+if not os.path.isfile(f'{sys.argv[1]}'):
     os.exit()
-vid = cv2.VideoCapture(f'./videos/{sys.argv[1]}')
+vid = cv2.VideoCapture(f'{sys.argv[1]}')
 timestamp = datetime.today().strftime('%Y%m%d%H%M%S')
 
-if not os.path.isdir(./gesture_data):
-    os.mkdir(f'./gesture_data')
+if not os.path.isdir('./gesture_data'):
+    os.mkdir('./gesture_data')
 os.mkdir(f'./gesture_data/{timestamp}')
-os.mkdir(f'./dataset/{timestamp}/left')
-os.mkdir(f'./dataset/{timestamp}/right')
+os.mkdir(f'./gesture_data/{timestamp}/left')
+os.mkdir(f'./gesture_data/{timestamp}/right')
 
 f = open(f'./splits/gesture/{sys.argv[2]}_files.txt',"a")
 
@@ -28,9 +28,9 @@ while True:
     imgL = frame[:,:width,:]
     imgR = frame[:,width:,:]
 
-    cv2.imwrite(f'./dataset/{timestamp}/left/{frame_i}.png',imgL)
+    cv2.imwrite(f'./gesture_data/{timestamp}/left/{frame_i}.png',imgL)
     f.write(f'{timestamp}/left {frame_i} l\n')
-    cv2.imwrite(f'./dataset/{timestamp}/right/{frame_i}.png',imgR)
+    cv2.imwrite(f'./gesture_data/{timestamp}/right/{frame_i}.png',imgR)
     f.write(f'{timestamp}/right {frame_i} r\n')
     frame_i += 1
 
